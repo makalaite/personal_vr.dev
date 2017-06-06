27a0f95b8fd4abf1357e5f212ef7b385d12ce06c
Agne <agnepetrylaite@gmail.com>
2017-06-06 15:27:30 +0300

    controllers was made

---
+++ app/Http/Controllers/SocialAuthController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Laravel\Socialite\Facades\Socialite;
+use App\Providers\SocialAccountService;
+
+class SocialAuthController extends Controller
+{
+
+}
---
+++ app/Http/Controllers/VRFrontEndController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\VRMenus;
+use App\Models\VRMenusTranslations;
+use App\Models\VRPages;
+use App\Models\VRPagesTranslations;
+use Illuminate\Http\Request;
+
+class VRFrontEndController extends Controller
+{
+    /**
+     * Gets data from menu and menu translations
+     * and sends it to home blade
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     *
+     */
+    public function displayMenu() {
+
+
+    }
+}
