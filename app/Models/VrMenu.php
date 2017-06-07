c1308d83632f46a736b661e9c0b7cec418ce9131
Agne <agnepetrylaite@gmail.com>
2017-06-07 10:08:33 +0300

    models

---
+++ app/Models/VrMenu.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+
+
+class VrMenu extends CoreModel
+{
+
+    /**
+     * Database table name
+     * @var string
+     */
+    protected $table = 'vr_menu';
+    /**
+     * Fillable column names
+     * @var array
+     */
+    protected $fillable = ['id', 'new_window', 'sequence', 'vr_parent_id'];
+}
