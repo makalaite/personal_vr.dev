2d6f527a6a154312453621dceb032e6ab00deaa6
Agne <agnepetrylaite@gmail.com>
2017-06-08 13:33:30 +0300

    lt translations

---
+++ resources/lang/lt/app.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'languages' => 'Kalba',
+    'orders' => 'Užsakymai',
+    'pages' => 'Puslapiai',
+    'categories' => 'Kategorijos',
+    'users' => 'Vartotojai',
+    'admin_menu' => 'Administratoriaus menu'
+];
