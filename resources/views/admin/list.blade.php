183319ffd6627682b9720dcc4f59e8138af80d1f
Agne <agnepetrylaite@gmail.com>
2017-06-08 13:32:56 +0300

    list blade

---
+++ resources/views/admin/list.blade.php
@@ -0,0 +1,7 @@
+@extends('admin.core')
+
+@section('content')
+<div id="list">
+Reikalai reikalai labai dideli reikalai.llllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll
+</div>
+    @endsection
