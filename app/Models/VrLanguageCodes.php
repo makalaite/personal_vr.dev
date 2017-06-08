11d24b94e53ae1c815fbea98bb98a6376d106395
Agne <agnepetrylaite@gmail.com>
2017-06-08 09:44:53 +0300

    languge codes model updated

--- app/Models/VrLanguageCodes.php
+++ app/Models/VrLanguageCodes.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class VrLanguageCodes extends Model
 {
-    use UuidTrait;
+
     public $incrementing = false;
     public $updated_at = false;
     /**
@@ -20,5 +20,5 @@ class VrLanguageCodes extends Model
      * Fillable column names
      * @var array
      */
-    protected $fillable = ['id', 'language_code'];
+    protected $fillable = ['id', 'language_code', 'name', 'is_active'];
 }
