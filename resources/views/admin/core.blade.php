8c6f25cd91c9d5cb24cd261f90feca9f32ac5625
Agne <agnepetrylaite@gmail.com>
2017-06-08 13:33:22 +0300

    core blade with bootstrap and included style

---
+++ resources/views/admin/core.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" href="{{asset('css/style.css')}}">
+
+    <title> Discover VR </title>
+</head>
+<body>
+
+@include('admin.menu')
+ @yield('content')
+
+</body>
+</html>
