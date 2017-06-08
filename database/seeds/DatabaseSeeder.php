d5cf35871f235a0b5c441a1aa84e8e4ec69d1b3b
Agne <agnepetrylaite@gmail.com>
2017-06-08 10:05:09 +0300

    language codes seeds

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\VrLanguageCodes;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,5 +13,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(VrRolesSeeder::class);
+        $this->call(VrLanguageCodesSeeder::class);
     }
 }
---
+++ database/seeds/VrLanguageCodesSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Models\VrLanguageCodes;
+use Illuminate\Database\Seeder;
+
+class VrLanguageCodesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds for language codes.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $langCodes = [
+
+            ["id" => "EN" , "language_code" => "EN", "name" => "English"],
+            ["id" => "LT" , "language_code" => "LT", "name" => "Lietuvių"],
+            ["id" => "RU" , "language_code" => "RU", "name" => "Русский"],
+            ["id" => "DE" , "language_code" => "DE", "name" => "Deutsch"],
+            ["id" => "FR" , "language_code" => "FR", "name" => "Français"],
+        ];
+        DB::beginTransaction();
+        try {
+            foreach ($langCodes as $langCode) {
+                $record = VrLanguageCodes::find($langCode['id']);
+                if (!$record) {
+                    VrLanguageCodes::create($langCode);
+                }
+            }
+        } catch (Exception $e) {
+            DB::rollback();
+            echo 'Point of failure '. $e->getCode() . ' ' . $e->getMessage();
+            throw new Exception($e);
+        }
+        DB::commit();
+    }
+}
