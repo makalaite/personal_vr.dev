2c868c9c12c292c98fb28546a3afd8179c025c58
Agne <agnepetrylaite@gmail.com>
2017-06-09 11:06:42 +0300

    jquery script

--- resources/views/admin/core.blade.php
+++ resources/views/admin/core.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" />
     <link rel="stylesheet" type="text/css" href="{{asset('css/style.css')}}">
 
@@ -10,9 +10,10 @@
 <body>
 
 @include('admin.menu')
- @yield('content')
+@yield('content')
 
 
 </body>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 @yield('scripts')
 </html>
