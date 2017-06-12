fd3cd4ccee9af1b5cec41b1549b35a3770696046
Agne <agnepetrylaite@gmail.com>
2017-06-12 14:26:04 +0300

    putting route in a base_path()

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        //dd(base_path('App/Http/helpers.php'));
+
+        require base_path('App/Http/helpers.php');
     }
 
     /**
