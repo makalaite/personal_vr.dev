41e33c05d9bd9ecb8c5a4c1d9b8d53ea3147d93c
Agne <agnepetrylaite@gmail.com>
2017-06-12 13:25:51 +0300

    more translations

--- resources/lang/lt/app.php
+++ resources/lang/lt/app.php
@@ -10,5 +10,9 @@ return [
     'no_data' => "Nėra duomenų",
     'activation' => 'Aktyvuoti',
     'disable' => 'Išjungti',
-    'language_codes_list' => 'Kalbų sąrašas'
+    'language_codes_list' => 'Kalbų sąrašas',
+    'category_list' => 'Kategorijų sąrašas',
+    'orders_list' => 'Užsakymų sąrašas',
+    'pages_list' => 'Puslapių sąrašas',
+    'users_list' => 'Vartotojų sąrašas',
 ];
