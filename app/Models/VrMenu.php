f0e824caec9cebd356cc5dc629f190dd3a6bea36
Agne <agnepetrylaite@gmail.com>
2017-06-14 14:34:12 +0300

    menu translation funkc

--- app/Models/VrMenu.php
+++ app/Models/VrMenu.php
@@ -18,5 +18,9 @@ class VrMenu extends CoreModel
      */
     protected $fillable = ['id', 'new_window', 'sequence', 'vr_parent_id'];
 
+    protected $with = ['translation'];
 
+    public function translation(){
+        return $this->hasOne(VrMenuTranslations::class, 'record_id', 'id')->where('language_code', app()->getLocale());
+    }
 }
