1edefd8daeb117bb2f5c184dbead3a44823f92b0
Agne <agnepetrylaite@gmail.com>
2017-06-14 14:36:21 +0300

    form blade edited

--- resources/views/admin/form.blade.php
+++ resources/views/admin/form.blade.php
@@ -1,26 +1,34 @@
 @extends('admin.core')
 
 @section('content')
-<h3>{{  $title  }} </h3>
+    <h3>{{  $title  }} </h3>
     {!! Form::open(['url' => $route]) !!}
 
     @foreach($fields as $field)
         <br/>
-        @if(isset($field['type'] == ))
-        {{ Form::label($field['key'], trans('app.' . $field['key'])) }}
+        {{ Form::label($field['key'], trans('app.' . $field['key'])) }} <br/>
 
         @if($field['type'] == 'single_line')
-<br/>
+
             {{ Form::text($field['key']) }}
-<br/>
-<br/>
+            <br/>
+
+
         @elseif ($field['type'] == 'drop_down')
 
             {!! Form::select($field['key'], $field['options'] ) !!}
+            <br/>
 
+        @elseif( ($field['type'] == 'check_box') )
+
+            @foreach($field['options'] as $option)
+                {{ Form::checkbox($option['name'], $option['value'])  }}
+                {{ Form::label($option['label'] ) }} <br/>
+            @endforeach
+            <br/>
         @endif
     @endforeach
-
+    <br/>
     {{ Form::submit('Patvirtinti'), array('class'=>'btn btn-success') }}
     {!! Form::close() !!}
 @endsection
