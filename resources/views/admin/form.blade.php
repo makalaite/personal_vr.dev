cf0ff51620253703f862b1636ad5db5da5ee210d
Agne <agnepetrylaite@gmail.com>
2017-06-15 13:45:24 +0300

    added place holder to form blade fot language drop down

--- resources/views/admin/form.blade.php
+++ resources/views/admin/form.blade.php
@@ -12,18 +12,24 @@
 
             {{ Form::text($field['key']) }}
             <br/>
-
-
         @elseif ($field['type'] == 'drop_down')
 
-            {!! Form::select($field['key'], $field['options'] ) !!}
-            <br/>
 
+            @if($field['key'] == 'language_code')
+
+                {!! Form::select($field['key'], $field['options'] ) !!}
+                <br/>
+            @else
+                {!! Form::select($field['key'], $field['options'], null, array('placeholder'=> '') ) !!}
+            @endif
+<br/>
         @elseif( ($field['type'] == 'check_box') )
 
             @foreach($field['options'] as $option)
+
                 {{ Form::checkbox($option['name'], $option['value'])  }}
                 {{ Form::label($option['label'] ) }} <br/>
+
             @endforeach
             <br/>
         @endif
@@ -31,4 +37,21 @@
     <br/>
     {{ Form::submit('Patvirtinti'), array('class'=>'btn btn-success') }}
     {!! Form::close() !!}
+@endsection
+
+@section('scripts')
+    <script>
+
+        $('#language_code').bind('change', function () {
+
+            //console.log( $('#language_code'). val())
+
+            //console.log( window.location.href = 'http://www.codeacademy.lt'); //iskart nukreipia i puslapi
+
+            // window.location.href = '/admin/menu' nukreips tiesiai i meniu lentele
+
+            window.location.href = '?language_code=' + $('#language_code').val(); // nurodai vieta kur jau esi, pridedi nekintama nuoroda plius id su reiksme ant kurios atsistosi
+
+        });
+    </script>
 @endsection
